2fa1584a17993670f21cc4b56e9bc467e0cf01aa
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-10-03 15:36:24 +0900

    Posts 등록 API 만들기 및 테스트

---
+++ spring-boot-board/src/main/java/com/board/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.board.springboot.service.posts;
+
+import com.board.springboot.domain.posts.PostsRepository;
+import com.board.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
