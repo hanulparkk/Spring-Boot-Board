2aba5bef1a4352668cdc84f47dcd038a9e7f7dd2
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-10-03 16:53:41 +0900

    Posts 수정, 조회 API 만들기 및 테스트

--- spring-boot-board/src/main/java/com/board/springboot/web/PostsApiController.java
+++ spring-boot-board/src/main/java/com/board/springboot/web/PostsApiController.java
@@ -1,9 +1,14 @@
 package com.board.springboot.web;
 
 import com.board.springboot.service.posts.PostsService;
+import com.board.springboot.web.dto.PostsResponseDto;
 import com.board.springboot.web.dto.PostsSaveRequestDto;
+import com.board.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +21,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
