3f634ad4791e86e910f8dc9dc2b0a3d3b7380c8b
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-10-04 21:40:02 +0900

    전체 조회 화면 만들기

--- spring-boot-board/src/main/java/com/board/springboot/service/PostsService.java
+++ spring-boot-board/src/main/java/com/board/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.board.springboot.service;
 
 import com.board.springboot.domain.posts.Posts;
 import com.board.springboot.domain.posts.PostsRepository;
+import com.board.springboot.web.dto.PostsListResponseDto;
 import com.board.springboot.web.dto.PostsResponseDto;
 import com.board.springboot.web.dto.PostsSaveRequestDto;
 import com.board.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
