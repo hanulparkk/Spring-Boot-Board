322f638278557655d70c66444a32de50eb209ad8
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-10-12 20:39:13 +0900

    세션 정보 값 가져오는 부분 - 어노테이션 기반으로 개선하기

--- spring-boot-board/src/main/java/com/board/springboot/web/IndexController.java
+++ spring-boot-board/src/main/java/com/board/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.board.springboot.web;
 
+import com.board.springboot.config.auth.LoginUser;
 import com.board.springboot.config.auth.dto.SessionUser;
 import com.board.springboot.service.PostsService;
 import com.board.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
